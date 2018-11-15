8ad602a0731edd04f0709635288d93e9ee155c1a
FelipeMira <felipe.mira@fatec.sp.gov.br>
2018-11-15 15:16:45 -0200

    ajuste botao pj e campo cpf

--- js/index.js
+++ js/index.js
@@ -10,25 +10,25 @@ var openMenu = function() {
   menu.classList.toggle('nav__list--active');
 };
 
-// reveal content of first panel by default
+// revela o conteudo do primeiro painel por default
 panel.eq(0).find('.panel__content').addClass('panel__content--active');
 
 var scrollFx = function() {
   var ds = doc.scrollTop();
   var of = vh / 4;
   
-  // if the panel is in the viewport, reveal the content, if not, hide it.
+  // se o painel estiver no viewport, revela o contaudo, se nao, esconde o mesmo.
   for (var i = 0; i < panel.length; i++) {
     if (panel.eq(i).offset().top < ds+of) {
-     panel
-       .eq(i)
-       .find('.panel__content')
-       .addClass('panel__content--active');
+      panel
+      .eq(i)
+      .find('.panel__content')
+      .addClass('panel__content--active');
     } else {
       panel
-        .eq(i)
-        .find('.panel__content')
-        .removeClass('panel__content--active')
+      .eq(i)
+      .find('.panel__content')
+      .removeClass('panel__content--active')
     }
   }
 };
@@ -39,11 +39,11 @@ var scrolly = function(e) {
   console.log(target);
   var $target = $(target);
   console.log($target);
-
+  
   $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
+    'scrollTop': $target.offset().top
   }, 300, 'swing', function () {
-      window.location.hash = target;
+    window.location.hash = target;
   });
 }
 
@@ -52,7 +52,35 @@ var init = function() {
   window.addEventListener('scroll', scrollFx, false);
   window.addEventListener('load', scrollFx, false);
   $('a[href^="#"]').on('click',scrolly);
-  $('svg[href^="#"]').on('click',scrolly);
 };
+
+$('#cpf').mask('999.999.999-99');
+
+function ValidaCPF(){	
+	var RegraValida=document.getElementById("cpf").value; 
+	var cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/;	 
+	if (cpfValido.test(RegraValida) == true)	{
+    console.log("CPF Válido");	
+	} else	{	 
+    console.log("CPF Inválido");	
+	}
+}
+function fMasc(objeto,mascara) {
+  obj=objeto
+  masc=mascara
+  setTimeout("fMascEx()",1)
+}
+
+function fMascEx() {
+  obj.value=masc(obj.value)
+}
+
+function mCPF(cpf){
+  cpf=cpf.replace(/\D/g,"")
+  cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+  cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+  cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+  return cpf
+}
 
 doc.on('ready', init);
